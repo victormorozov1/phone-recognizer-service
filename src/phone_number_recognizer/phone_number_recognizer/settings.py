67dc24995908e3ae862f64dd99d36dbc29a69ffb
victormorozov1 <vismo@bk.ru>
2025-04-21 12:00:04 +0700

    change structure and add front

--- src/phone_number_recognizer/phone_number_recognizer/settings.py
+++ src/phone_number_recognizer/phone_number_recognizer/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
 
     'api',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Not for prod!!!
+CORS_ALLOW_ALL_ORIGINS = True
